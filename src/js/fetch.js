a9965849b1df4bcc1b482d8cc20a10d3d6c7c15e
HeinrichVannas <heinivannas@gmail.com>
2024-10-31 15:27:07 +0200

    Made that the posts are read in from a json file.

---
+++ src/js/fetch.js
@@ -0,0 +1,45 @@
+window.onload = function() {
+// ../../res/json/posts.json
+
+    fetch('../../res/json/posts.json')
+        .then((response) => response.json())
+        .then(json => {
+            console.log(json);
+            for(i = 0; i < json.length; i++){
+                let post = document.createElement("div");
+                let postHead = document.createElement("div");
+                let date = document.createElement("p");
+                let postMessage = document.createElement("p");
+                let img = document.createElement("img")
+
+                let defaultPic = document.createElement("img");
+                let likePic = document.createElement("img");
+
+                defaultPic.src = "../../images/default_profile.jpg";
+                likePic.src = "../../images/likeIcon.png";
+                likePic.className = "like"
+
+                post.className = "post";
+                postHead.className = "postHead";
+
+                date.className = "date";
+                date.innerText = json[i].postDate
+
+                postMessage.className = "postMessage";
+                postMessage.innerText = json[i].message
+
+                postHead.appendChild(defaultPic);
+                postHead.appendChild(date)
+
+                post.appendChild(postHead);
+                if(json[i].image != null){
+                    img.src = json[i].image
+                    post.appendChild(img);
+                }
+                post.appendChild(postMessage);
+                post.appendChild(likePic);
+
+                document.getElementById("posts").appendChild(post)
+            }
+        })
+}
