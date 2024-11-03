550a42ab19d2c2d1e75a44980df3784f1756c395
HeinrichVannas <55141996+HeinrichVannas@users.noreply.github.com>
2024-11-03 20:17:50 +0200

    Update fetch.js
    Changed wrong path

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -3,7 +3,7 @@ window.onload = function() {
     
     //fetch('https://api.jsonsilo.com/demo/2c479ed1-7c9c-464f-9967-c7a919108dc6')
 
-    fetch('../../res/json/posts.json')
+    fetch('../res/json/posts.json')
         .then((response) => response.json())
         .then(json => {
             console.log(json);
